f9cca14fdd4365784358fb354153621c103669c5
Paolo <parcaini@gmail.com>
2017-02-24 16:22:54 +0100

    h

--- mutrex/src/regex/operators/CharacterClassAddition.java
+++ mutrex/src/regex/operators/CharacterClassAddition.java
@@ -18,16 +18,25 @@ import dk.brics.automaton.oo.ooregex;
 public class CharacterClassAddition extends RegexMutator {
 	public static CharacterClassAddition mutator = new CharacterClassAddition();
 	private static Logger logger = Logger.getLogger(CharacterClassAddition.class.getName());
-	private static REGEXP_CHAR_RANGE[] intervals = new REGEXP_CHAR_RANGE[] {
+	/*private static REGEXP_CHAR_RANGE[] intervals = new REGEXP_CHAR_RANGE[] {
 			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[a-z]")),
 			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[A-Z]")),
-			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[0-9]")) };
+			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[0-9]")) };*/
+	
+	private static REGEXP_CHAR_RANGE[] ALLintervals = new REGEXP_CHAR_RANGE[] {
+			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[a-z]")),
+			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[A-Z]")),
+			(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[0-9]")) };	
 
 	private CharacterClassAddition() {
 		super(new CharacterClassAdditionVisitor());
 	}
 
 	static class CharacterClassAdditionVisitor extends RegexVisitorAdapterList {
+		private REGEXP_CHAR_RANGE[] intervals = new REGEXP_CHAR_RANGE[] {
+				(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[a-z]")),
+				(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[A-Z]")),
+				(REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp("[0-9]")) };
 
 		/* (non-Javadoc)
 		 * @see regexrepair.operators.RegexVisitorAdapterList#visit(dk.brics.automaton.oo.REGEXP_CHAR_RANGE)
@@ -46,6 +55,28 @@ public class CharacterClassAddition extends RegexMutator {
 			}
 			return result;
 		}
+
+		/*@Override
+		public List<ooregex> visit(REGEXP_UNION r) {
+			List<ooregex> parts = REGEXP_UNION.splitUnion(r);
+			List<String> intrs = new ArrayList<String>();
+			intrs.add("[0-9]");
+			intrs.add("[a-z]");
+			intrs.add("[A-Z]");
+			for(ooregex p: parts) {
+				intrs.remove(p.toString());
+			}
+			intervals = new REGEXP_CHAR_RANGE[intrs.size()];
+			for(int i = 0; i < intrs.size(); i++) {
+				intervals[i] = (REGEXP_CHAR_RANGE) OORegexConverter.getOORegex(new RegExp(intrs.get(i)));
+			}
+			ArrayList<ooregex> result = new ArrayList<ooregex>();
+			for(ooregex p: parts) {
+				result.addAll(p.accept(this));
+			}
+			intervals = ALLintervals;
+			return result;
+		}*/
 
 		@Override
 		public String getCode() {
