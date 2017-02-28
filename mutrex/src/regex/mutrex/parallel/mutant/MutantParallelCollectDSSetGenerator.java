2f82f802986c4e747e2f2737fa5893c5aeb5eedc
Paolo <parcaini@gmail.com>
2017-02-28 12:34:10 +0100

    i

--- mutrex/src/regex/mutrex/parallel/mutant/MutantParallelCollectDSSetGenerator.java
+++ mutrex/src/regex/mutrex/parallel/mutant/MutantParallelCollectDSSetGenerator.java
@@ -2,29 +2,22 @@ package regex.mutrex.parallel.mutant;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import dk.brics.automaton.Automaton;
 import dk.brics.automaton.RegExp;
-import regex.distinguishing.DSgenPolicy;
-import regex.distinguishing.DistStringCreator;
 import regex.mutrex.ds.DSSet;
 import regex.mutrex.ds.DSSetGenerator;
 import regex.mutrex.ds.DistinguishingAutomaton;
 import regex.mutrex.ds.DistinguishingAutomaton.RegexWAutomata;
-import regex.mutrex.parallel.DistinguishingAutomatonTh;
-import regex.operators.RegexMutator;
 import regex.operators.RegexMutator.MutatedRegExp;
 
 public class MutantParallelCollectDSSetGenerator extends DSSetGenerator {
 	private static Logger logger = Logger.getLogger(MutantParallelCollectDSSetGenerator.class.getName());
+	public static DSSetGenerator generator = new MutantParallelCollectDSSetGenerator();
 	int numRunningMutants = 0;
 
 	public synchronized Mutant getMutant(Iterator<MutatedRegExp> mutants) {
@@ -38,20 +31,22 @@ public class MutantParallelCollectDSSetGenerator extends DSSetGenerator {
 				continue;
 			} else {
 				numRunningMutants++;
-				return mutants.next();
+				return new Mutant(mutants.next());
 			}
 		}
 		return null;
 	}
 
+	public synchronized void decreaseRunningMuts() {
+		numRunningMutants--;
+	}
+
 	@Override
 	public void addStringsToDSSet(DSSet dsS, RegExp regex, Iterator<MutatedRegExp> mutants) {
-		
-		RegexWAutomata regexWithAutomata = new RegexWAutomata(regex);
-		DasManager dasManager = new DasManager();
+		DasManager dasManager = new DasManager(regex);
 		Mutant mutant = null;
-loopMut:while ((mutant = getMutant(mutants)) != null) {
-			
+		while ((mutant = getMutant(mutants)) != null) {
+			new MutTh(mutant, dasManager, this).start();
 		}
 	}
 }
@@ -59,10 +54,12 @@ loopMut:while ((mutant = getMutant(mutants)) != null) {
 class MutTh extends Thread {
 	Mutant mutant;
 	DasManager dasManager;
+	MutantParallelCollectDSSetGenerator gen;
 
-	public MutTh(Mutant mutant, DasManager dasManager) {
+	public MutTh(Mutant mutant, DasManager dasManager, MutantParallelCollectDSSetGenerator gen) {
 		this.mutant = mutant;
 		this.dasManager = dasManager;
+		this.gen = gen;
 	}
 	
 	@Override
@@ -70,11 +67,11 @@ class MutTh extends Thread {
 		List<Boolean> trueFalse = Arrays.asList(true, false);
 		boolean isCovered = false;
 		while(!isCovered) {
-			DistinguishingAutomatonClass da = dasManager.getDA(mutant);
-			if(da == null) {
+			DistinguishingAutomatonClass dac = dasManager.getDA(mutant);
+			if(dac == null) {
 				Collections.shuffle(trueFalse);
 				for (boolean b : trueFalse) {
-					DistinguishingAutomaton newDa = new DistinguishingAutomaton(mutant.mutant, b);
+					DistinguishingAutomaton newDa = new DistinguishingAutomaton(dasManager.regexWithAutomata, b);
 					if (newDa.add(mutant.mutant)) {
 						DistinguishingAutomatonClass newDaC = new DistinguishingAutomatonClass(newDa);
 						dasManager.addDA(newDaC);
@@ -85,9 +82,13 @@ class MutTh extends Thread {
 				mutant.isEquivalent = true;
 			}
 			else {
-				
+				if(dac.da.add(mutant.mutant)) {
+					isCovered = true;
+				}
+				dasManager.unlock(dac);
 			}
 		}
+		gen.decreaseRunningMuts();
 	}
 	
 }
@@ -95,22 +96,45 @@ class MutTh extends Thread {
 class DasManager {
 	Set<DistinguishingAutomatonClass> daS = new HashSet<DistinguishingAutomatonClass>();
 	List<Boolean> trueFalse = Arrays.asList(true, false);
+	RegexWAutomata regexWithAutomata;
+
+	public DasManager(RegExp regex) {
+		regexWithAutomata = new RegexWAutomata(regex);
+	}
 
 	public synchronized void addDA(DistinguishingAutomatonClass da) {
 		daS.add(da);
 	}
+
+	public synchronized void unlock(DistinguishingAutomatonClass da) {
+		da.locked = false;
+		notifyAll();
+	}
 	
 	public synchronized DistinguishingAutomatonClass getDA(Mutant mutant) {
 		Set<DistinguishingAutomatonClass> checkedByMut = mutant.visited;
-		for (DistinguishingAutomatonClass da : daS) {
-			if (!checkedByMut.contains(da)) {
-				if (!da.locked) {
-					da.locked = true;
-					return da;
+		while(true) {
+			boolean addNewDa = true;
+			for (DistinguishingAutomatonClass da : daS) {
+				if (!checkedByMut.contains(da)) {
+					addNewDa = false;
+					if (!da.locked) {
+						da.locked = true;
+						return da;
+					}
+				}
+			}
+			if(addNewDa) {
+				return null;
+			}
+			else {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
 		}
-		return null;
 	}
 }
 
