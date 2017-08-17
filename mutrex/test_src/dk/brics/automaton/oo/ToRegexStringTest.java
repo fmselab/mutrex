a28bc442e84eb2a95d8225e0159a843042ca4468
Angelo Gargantini <angelo.gargantini@unibg.it>
2017-08-17 18:27:42 -0400

    eliminato 1.11

---
+++ mutrex/test_src/dk/brics/automaton/oo/ToRegexStringTest.java
@@ -0,0 +1,20 @@
+package dk.brics.automaton.oo;
+
+import org.junit.Test;
+
+import dk.brics.automaton.OORegexConverter;
+import dk.brics.automaton.RegExp;
+
+public class ToRegexStringTest {
+
+	@Test
+	public void test() {
+		// very strange problems with --help
+		// if its contains a doubel quote, it must be escaped !!!
+		ooregex r = OORegexConverter.getOOExtRegex("\"a");
+		String s = ToRegexString.convertToRegexString(r);
+		System.out.println(s);
+		RegExp newRegex = new RegExp(s);
+	}
+
+}
