5f4976ee80e3ff17e4d8a73946564a4744d5a724
Angelo Gargantini <angelo.gargantini@unibg.it>
2017-08-18 12:54:41 -0400

    mintest new version

--- mutrex/src/regex/mutrex/minimization/MinTest.java
+++ mutrex/src/regex/mutrex/minimization/MinTest.java
@@ -2,9 +2,11 @@ package regex.mutrex.minimization;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
 
 import dk.brics.automaton.Automaton;
 import dk.brics.automaton.RegExp;
@@ -13,10 +15,6 @@ import regex.mutrex.ds.DSSet;
 import regex.mutrex.ds.RegExpSet;
 import regex.operators.RegexMutator.MutatedRegExp;
 
-/**
- * minimize the DSSet using mintest as in the paper written by Ammann et  alt.
- *
- */
 public class MinTest extends DSSetMinimizer{
 
 	public static MinTest instance = new MinTest();
@@ -26,35 +24,47 @@ public class MinTest extends DSSetMinimizer{
 	}
 	@Override
 	public void minimize(DSSet dsS, RegExp regex) {
-		int initSize = dsS.size();
-		Random rnd = new Random();
-		Automaton regexAut = regex.toAutomaton();
-		while (initSize > 0) {
-			DistinguishingString currDS = dsS.getDS(rnd.nextInt(dsS.size()));
-			RegExpSet killedMutants = dsS.getKilledMutants(currDS);
-			Map<RegExp, DistinguishingString> coveredBy = new HashMap<RegExp, DistinguishingString>();
-			for (RegExp currDsMutant : killedMutants) {
-				Automaton currDsMutantAut = currDsMutant.toAutomaton();
-				Automaton simmDiff = currDsMutantAut.minus(regexAut).union(regexAut.minus(currDsMutantAut));
-				for (DistinguishingString ds : dsS) {
-					if (ds != currDS) {
-						if (simmDiff.run(ds.getDs())) {
-							coveredBy.put(currDsMutant, ds);
-							break;
+			int initSize = dsS.size();
+			Random rnd = new Random();
+			Map<RegExp, Set<DistinguishingString>> killedBy = new HashMap<RegExp, Set<DistinguishingString>>();
+			while (initSize > 0) {
+				DistinguishingString currDS = dsS.getDS(rnd.nextInt(dsS.size()));
+				RegExpSet killedMutants = dsS.getKilledMutants(currDS);
+				Map<RegExp, DistinguishingString> coveredBy = new HashMap<RegExp, DistinguishingString>();
+				for (RegExp currDsMutant : killedMutants) {
+					Automaton currDsMutantAut = currDsMutant.toAutomaton();
+					for (DistinguishingString ds : dsS) {
+						if (ds != currDS) {
+							Set<DistinguishingString> killedBySet = killedBy.get(currDsMutant);
+							if(killedBySet != null && killedBySet.contains(ds)) {
+								coveredBy.put(currDsMutant, ds);
+								break;
+							}
+							boolean acceptedByRegex = ds.isConfirming();
+							boolean acceptedByMut = currDsMutantAut.run(ds.getDs());
+							if (acceptedByRegex != acceptedByMut) {
+								coveredBy.put(currDsMutant, ds);
+								if(killedBySet == null) {
+									killedBySet = new HashSet<DistinguishingString>();
+									killedBy.put(currDsMutant, killedBySet);
+								}
+								killedBySet.add(ds);
+								break;
+							}
 						}
 					}
 				}
-			}
-			if (coveredBy.size() == killedMutants.size()) {
-				for (Entry<RegExp, DistinguishingString> entry : coveredBy.entrySet()) {
-					dsS.add(entry.getValue(), Collections
-							.singletonList(new MutatedRegExp(String.valueOf(rnd.nextInt()), entry.getKey())));// to
-																												// fix
+				if (coveredBy.size() == killedMutants.size()) {
+					for (Entry<RegExp, DistinguishingString> entry : coveredBy.entrySet()) {
+						dsS.add(entry.getValue(), Collections
+								.singletonList(new MutatedRegExp(String.valueOf(rnd.nextInt()), entry.getKey())));// to
+																													// fix
+					}
+					dsS.remove(currDS);
 				}
-				dsS.remove(currDS);
+				initSize--;
 			}
-			initSize--;
 		}
-	}
-
+	
 }
+
