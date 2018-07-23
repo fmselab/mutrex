53cfdd1ccbe9f91379a7c000262a85869664becd
Paolo Arcaini <parcaini@gmail.com>
2018-07-23 15:34:35 +0900

    test

--- mutrex/test_src/regex/operators/AllMutatorsTest.java
+++ mutrex/test_src/regex/operators/AllMutatorsTest.java
@@ -16,10 +16,8 @@ import dk.brics.automaton.oo.ToSimpleString;
 import regex.operators.RegexMutator.MutatedRegExp;
 
 public class AllMutatorsTest {
-
 	RegexComparatorByAutomata comparator = new RegexComparatorByAutomata();
-	
-	
+
 	@Test
 	public void testMutate() {
 		Logger.getLogger(OORegexConverter.class.getName()).setLevel(Level.OFF);
@@ -34,7 +32,7 @@ public class AllMutatorsTest {
 		Logger.getLogger(OORegexConverter.class.getName()).setLevel(Level.OFF);
 		testWithRegex("[a-z]{2,3}");
 	}
-	
+
 	@Test
 	public void testMutate4() {
 		Logger.getLogger(OORegexConverter.class.getName()).setLevel(Level.OFF);
@@ -43,7 +41,7 @@ public class AllMutatorsTest {
 		testWithRegex("a*");
 		testWithRegex("\\w+\\@[a-zA-Z_]");
 	}
-	
+
 	// every mutation can be translated back
 	private void testWithRegex(String start) {
 		// build the regexp
@@ -52,7 +50,7 @@ public class AllMutatorsTest {
 		// build mutations
 		Iterator<MutatedRegExp> mutants = AllMutators.mutator.mutateRandom(correct);
 		// for each mutation is possible to mutate and go back to correct
-		nextMut: while(mutants.hasNext()) {
+		nextMut: while (mutants.hasNext()) {
 			MutatedRegExp mut = mutants.next();
 			String mutS = ToSimpleString.convertToReadableString(mut.mutatedRexExp);
 			// now mutate this one
@@ -60,27 +58,28 @@ public class AllMutatorsTest {
 			// for each mutation is possible
 			String considered = "";
 			String mtmtString = "";
-			while(mtsmts.hasNext()) {
+			while (mtsmts.hasNext()) {
 				MutatedRegExp mutmut = mtsmts.next();
 				RegExp mtmt = mutmut.mutatedRexExp;
 				mtmtString = ToSimpleString.convertToReadableString(mtmt);
-				considered += mtmtString + "[" +mutmut.description + "]";
-				considered += "{"+ OORegexConverter.getOORegex(mutmut.mutatedRexExp).getClass().getSimpleName() + "}";
+				considered += mtmtString + "[" + mutmut.description + "]";
+				considered += "{" + OORegexConverter.getOORegex(mutmut.mutatedRexExp).getClass().getSimpleName() + "}";
 				considered += ",";
 				// if found equal
 				// not working: mtmt.equals(correct)
 				// not working with automata omparator.compare(mtmt, correct) == 0
 				// with strings
 				if (mtmtString.equals(correctStr)) {
-					System.out.println("this mutation " + mut.description+ " " + mutS + " can be translated back to "+ correctStr + " by " + mutmut.description);					
+					System.out.println("this mutation " + mut.description + " " + mutS + " can be translated back to "
+							+ correctStr + " by " + mutmut.description);
 					continue nextMut;
 				}
-			}	
+			}
 			// error found
 			System.err.println(OORegexConverter.getOORegex(correct).getClass());
-			System.err.println("this mutation " + mut.description+ " " + mutS + " cannot translated back to " + correctStr);
+			System.err.println(
+					"this mutation " + mut.description + " " + mutS + " cannot translated back to " + correctStr);
 			System.err.println("mutatants considered " + considered);
 		}
 	}
-
 }
