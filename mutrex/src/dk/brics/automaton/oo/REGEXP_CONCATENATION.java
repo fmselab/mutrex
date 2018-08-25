14486ed383cc80c1c18051753d89b53a4b01c780
Paolo Arcaini <parcaini@gmail.com>
2018-08-25 11:33:28 +0200

    a

--- mutrex/src/dk/brics/automaton/oo/REGEXP_CONCATENATION.java
+++ mutrex/src/dk/brics/automaton/oo/REGEXP_CONCATENATION.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import dk.brics.automaton.RegExp;
-
 /** a copncatenation bewteen two ooregexes */
 public class REGEXP_CONCATENATION extends oobinregex {
 
